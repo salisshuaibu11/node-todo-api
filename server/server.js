8be0d09449076f51e89f1971d01b6d62876c2a52
Salisu <salisshuaibu11@gmail.com>
2021-04-19 10:41:55 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -16,7 +17,7 @@ app.post("/todos", (req, res) => {
 
   todo.save().then(
     (doc) => {
-      res.send(doc);
+      res.status(200).send(doc);
     },
     (e) => {
       res.status(400).send(e);
@@ -28,6 +29,26 @@ app.get("/todos", (req, res) => {
   Todo.find()
     .then((todos) => {
       res.send({ todos });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
     })
     .catch((e) => {
       res.status(400).send(e);
