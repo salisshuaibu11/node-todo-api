cc5bba5d0fc0edbc387e2c57dfdbe382d8eb9124
Salisu <salisshuaibu11@gmail.com>
2021-04-19 09:55:14 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,18 @@ app.post("/todos", (req, res) => {
   );
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({ todos });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+module.exports = app;
